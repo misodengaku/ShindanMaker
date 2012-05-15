d849f3c7dac39b06ffa7807cc79a75a939aa3c9d
misodengaku <misodengaku@gmail.com>
2012-05-16 01:31:49 +0900

    First Commit

---
+++ ShindanMaker/Method.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Xml.Linq;
+using Sgml;
+using System.Text;
+using System.Windows;
+
+namespace ShindanMaker
+{
+    public static class Method
+    {
+        public static void GetShindan(int id, Action<object> act)
+        {
+            var wc = new WebClient();
+            wc.OpenReadAsync(new Uri("http://shindanmaker.com/"+id));
+            wc.OpenReadCompleted += (sender, e) =>
+            {
+                if (e.Error != null)
+                {
+                    return;
+                }
+
+                using (var reader = new StreamReader(e.Result, System.Text.Encoding.UTF8))
+                using (var sgmlReader = new SgmlReader { InputStream = reader })
+                {
+                    sgmlReader.DocType = "HTML";
+                    sgmlReader.CaseFolding = CaseFolding.ToLower;
+
+                    var doc = XElement.Load(sgmlReader);
+                    var ns = doc.Name.Namespace;
+                    var q = from ele in doc.Descendants(ns + "body")
+                            .Elements(ns + "div")
+                            .Where(ul => ul.Attribute("class") != null
+                                && ul.Attribute("class").Value == "wrap1")
+                            .Elements(ns + "div")
+                            .Elements(ns + "div")
+                            .Skip(1)
+                            .Elements(ns + "div")
+                            select new ShindanItem
+                            {
+                                Text = ele.Value//Element(ns + "div").Element(ns + "h1").Value,
+                            };
+                    var re = q.First<ShindanItem>();
+                    char[] sep = { ' ', '\n', '\t' };
+                    re.Title = re.Text.Split(sep, StringSplitOptions.RemoveEmptyEntries)[0];
+                    re.Description = re.Text.Split(sep, StringSplitOptions.RemoveEmptyEntries)[1];
+                    act(re);
+                    
+                }
+
+            };
+        }
+
+        public static void ExecShindan(int id, string name, Action<string> act)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create("http://shindanmaker.com/"+id);
+            webRequest.Method = "POST";
+            webRequest.ContentType = "multipart/form-data";
+
+            // Start the request
+            webRequest.BeginGetRequestStream(r =>
+            {
+                try
+                {
+                    HttpWebRequest request1 = (HttpWebRequest)r.AsyncState;
+                    Stream postStream = request1.EndGetRequestStream(r);
+
+
+                    string postData = "&u=" + HttpUtility.UrlEncode(name)+"&from=";
+                    byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+                    postStream.Write(byteArray, 0, byteArray.Length);
+                    postStream.Close();
+
+                    request1.BeginGetResponse(
+                        s =>
+                        {
+                            try
+                            {
+                                HttpWebRequest request2 = (HttpWebRequest)s.AsyncState;
+                                HttpWebResponse response = (HttpWebResponse)request2.EndGetResponse(s);
+
+                                Stream streamResponse = response.GetResponseStream();
+                                StreamReader streamReader = new StreamReader(streamResponse);
+                                string response2 = streamReader.ReadToEnd();
+                                streamResponse.Close();
+                                streamReader.Close();
+                                response.Close();
+                                //act("hoge");
+                                act(response2);
+                                return;
+                            }
+                            catch
+                            {
+                            }
+                        },
+                    request1);
+                }
+                catch
+                {
+                }
+            }, webRequest);
+
+
+        }
+    }
+}
---
+++ meshiyosoi/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using ShindanMaker;
+
+namespace meshiyosoi
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // コンストラクター
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Method.GetShindan(int.Parse(textBox1.Text), ec =>
+            {
+                var item = (ShindanItem)ec;
+                //MessageBox.Show("Title:"+item.Title+"\nDescription:"+item.Description);
+                textBlock2.Text = "診断名：" + item.Title;
+                textBlock3.Text = "説明：" + item.Description;
+            });
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            Method.ExecShindan(int.Parse(textBox1.Text), textBox2.Text, result =>
+                {
+                    Dispatcher.BeginInvoke(() => MessageBox.Show(result.ToString()));
+                });
+        }
+    }
+}
